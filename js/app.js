3eec354c0abec11c10accb0258edc11baec64c43
Sara Bento de Castro <denominatu@hotmail.com>
2018-04-21 17:33:44 +0200

    declared variables and wrote functions

--- js/app.js
+++ js/app.js
@@ -1,5 +1,16 @@
+
 // Enemies our player must avoid
-var Enemy = function() {
+let allEnemies = [];
+let canvasWidth = 1010;
+let canvasHeight = 830;
+
+
+const Enemy = function(x, y, speed, sprite) {
+    this.x = x;
+    this.y = y;
+    this.speed = getRandomInt(100, 220);
+
+
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
@@ -8,9 +19,29 @@ var Enemy = function() {
     this.sprite = 'images/enemy-bug.png';
 };
 
+(function createEnemies() {
+    const bug1 = new Enemy (20, 10, 20, 10);
+    const bug2 = new Enemy (200, 100, 200, 100);
+    const bug3 = new Enemy (340, 310, 200, 100);
+    const bug4 = new Enemy (200, 100, 20, 10);
+    allEnemies.push(bug1, bug2, bug3, bug4);
+})();
+
+
+
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
+
+  this.x += this.speed * dt;
+
+
+    // When enemy goes off the canvas, bring it back on the other side
+    if (this.x <= - 50) {
+        this.x = canvasWidth + 50;
+    } else if (this.x >= canvasWidth + 50) {
+        this.x = -50;
+}
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
@@ -21,6 +52,50 @@ Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
+//PLayer
+
+const Player = function(x,y, width, height,sprite) {
+    this.x = x;
+    this.y = y;
+    this.sprite = "images/char-boy.png";
+    this.width = 101;
+    this.height = 171;
+
+
+
+
+};
+
+const player = new Player (0,0);
+
+Player.prototype.render = function(){
+  ctx.drawImage(Resources.get(this.sprite),
+
+  this.x + (canvasWidth*.5),
+  this.y + (canvasHeight-200),
+);
+}
+
+Player.prototype.update = function(){
+
+}
+
+Player.prototype.handleInput = function(key){
+  switch (key) {
+      case 'up':
+          this.y -= 80;
+          break;
+      case 'down':
+          this.y += 80;
+          break;
+      case 'left':
+          this.x -= 100;
+          break;
+      case 'right':
+          this.x += 100;
+          break;
+};
+}
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
@@ -44,3 +119,24 @@ document.addEventListener('keyup', function(e) {
 
     player.handleInput(allowedKeys[e.keyCode]);
 });
+
+
+let collision = contains(Player, Enemy);
+
+
+
+// test for collision player and enemey
+function contains(collisionBounds, target) {
+  return (
+         );
+}
+
+
+
+
+// random integer from MDN
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+}
